07543782bbb443cda914cf756b8f1f78a2adb200
Melvin Chang <melvinchang21@gmail.com>
2023-07-25 14:49:28 -0700

    deleted unneccesary line

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const { default: mongoose } = require('mongoose');
 const PORT = 4000;
 
 app.use(cors());
