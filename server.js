3ed9af04c8685eb2951da6d73a2a290a4af2ad28
Melvin Chang <melvinchang21@gmail.com>
2023-07-27 17:08:34 -0700

    added delete endpoint

--- server.js
+++ server.js
@@ -9,16 +9,20 @@ const PORT = 4000;
 let Todo = require('./todo.model');
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Use express.json() middleware for parsing JSON data
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
-const connection = mongoose.connection;
+// Use async/await for database connection
+(async () => {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+        console.log("MongoDB database connection established successfully");
+    } catch (err) {
+        console.error("MongoDB connection error:", err.message);
+        process.exit(1); // Exit the application on connection failure
+    }
+})();
 
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
-});
-
-todoRoutes.route('/').get(async function (req, res) {
+todoRoutes.route('/').get(async (req, res) => {
     try {
         const todos = await Todo.find(); // Use await to wait for the Promise to resolve
         res.json(todos);
@@ -28,7 +32,7 @@ todoRoutes.route('/').get(async function (req, res) {
     }
 });
 
-todoRoutes.route('/:id').get(async function (req, res) {
+todoRoutes.route('/:id').get(async (req, res) => {
     try {
         let id = req.params.id;
         const todo = await Todo.findById(id);
@@ -39,18 +43,18 @@ todoRoutes.route('/:id').get(async function (req, res) {
     }
 });
 
-todoRoutes.route('/add').post(async function (req, res) {
+todoRoutes.route('/add').post(async (req, res) => {
     try {
         let todo = new Todo(req.body);
         await todo.save();
-        res.status(200).json({ 'todo': "Todo added successfully"});
+        res.status(200).json({ 'todo': "Todo added successfully" });
     } catch (err) {
         console.log(err);
         res.status(400).send("Adding new Todo failed");
     }
 });
 
-todoRoutes.route('/update/:id').post(async function (req, res) {
+todoRoutes.route('/update/:id').post(async (req, res) => {
     try {
         const todo = await Todo.findById(req.params.id);
         if (!todo) {
@@ -60,7 +64,7 @@ todoRoutes.route('/update/:id').post(async function (req, res) {
             todo.todo_responsible = req.body.todo_responsible;
             todo.todo_priority = req.body.todo_priority;
             todo.todo_completed = req.body.todo_completed;
-            
+
             await todo.save();
             res.json("Todo updated");
         }
@@ -70,8 +74,24 @@ todoRoutes.route('/update/:id').post(async function (req, res) {
     }
 })
 
+todoRoutes.route('/:id').delete(async (req, res) => {
+    try {
+        const todoID = req.params.id;
+        const todo = await Todo.findByIdAndDelete(todoID);
+
+        if (!todo) {
+            res.status(404).json({ error: "Data not found" });
+        } else {
+            res.json( { message: "Todo deleted successfully" } );
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
 app.use('/todos', todoRoutes);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
 });
